c0d6cf7ff3126be647c321b42710cd2ddaf8f343
Iadolphe007 <iadolphe007@gmail.com>
2024-01-24 16:40:59 +0000

    int

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 import random
 import sys
-from datetime import datetime
 from time import sleep
+import datetime
 
-# Generate random log lines in expected format
 for i in range(10000):
-    ip_address = ".".join(str(random.randint(1, 255)) for _ in range(4))
-    timestamp = datetime.now().strftime("%d/%b/%Y:%H:%M:%S %z")
-    method = "GET"
-    resource = "/projects/260"
-    protocol = "HTTP/1.1"
-    status_code = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
-    file_size = random.randint(1, 1024)
-    log_line = "{} - [{}] \"{} {} {}\" {} {}\n".format(ip_address, timestamp, method, resource, protocol, status_code, file_size)
-    sys.stdout.write(log_line)
-    sys.stdout.flush()
     sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
