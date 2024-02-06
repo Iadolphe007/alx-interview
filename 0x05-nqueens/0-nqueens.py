a3557756f6f0a1b8999af399f4b7dbe7c2e99904
Iadolphe007 <iadolphe007@gmail.com>
2024-02-06 10:07:08 +0000

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,22 +3,6 @@
 import sys
 
 
-args = sys.argv
-
-if len(args) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-if args[1].isdigit() is False:
-    print("N must be a number")
-    exit(1)
-
-layout = int(args[1])
-if layout < 4:
-    print("N must be at least 4")
-    exit(1)
-
-
 def check_here(nlist, index, curr_num):
     for item in nlist:
         if index == item[1] or\
@@ -28,7 +12,7 @@ def check_here(nlist, index, curr_num):
     return list([curr_num, index])
 
 
-def loop_n_find(nlist, curr_num):
+def loop_n_find(nlist, curr_num, layout):
     if (curr_num > layout):
         return
 
@@ -42,11 +26,31 @@ def loop_n_find(nlist, curr_num):
         if type(val) is list:
             new_nlist = nlist.copy()
             new_nlist.append(val)
-            loop_n_find(new_nlist, curr_num + 1)
+            loop_n_find(new_nlist, curr_num + 1, layout)
         j += 1
     return
 
 
-for i in range(layout):
-    arr = [[0, i]]
-    loop_n_find(arr, 1)
+def solve_nqueens(layout):
+    for i in range(layout):
+        arr = [[0, i]]
+        loop_n_find(arr, 1, layout)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    if len(args) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    if not args[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    layout = int(args[1])
+    if layout < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(layout)
